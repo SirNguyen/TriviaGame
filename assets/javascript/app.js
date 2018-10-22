5b76fdffd9f1f2e9f7726ea4d47dbc28654c08b8
SirNguyen <nguyentimvan@gmail.com>
2018-10-22 11:50:43 -0700

    added some details for the files

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var corr = 0;
+var wrg = 0;
+var inc = 0;
+var totalQuestions = 10;
+var time = 0;
+var answerButtons = [];
+var newQuestions = [];
+
+$(document).ready(function() {
+    newQuiz();
+    play();
+}
+
+function play() {
+    
+}
