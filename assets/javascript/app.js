c3a2bb5bb769190063473122dfd3309086ee72b2
Tim Nguyen <nguyentimvan@gmail.comgitconfig --global user.name Tim>
2018-11-21 11:22:54 -0800

    added photos and js functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,94 @@
-/**
- * RADIO INPUT
- * submit button event handler
- * when submit is pressed, get radio input value
- * store radio input in an object
- * compare the chosen values to the correct values
- * display correct and incorrect answers with results
- * 
-*/
+// Variables
 
+var trivia = {
+  initialScreen: "",
+  correctCounter: 0,
+  inCorrectCounter: 0,
+  unAnsweredCounter: 0,
+  gameHTML: "",
+  questionsArray: [
+                  "Whose entire skeletal system was fused with the metal adamantium?", "T'Challa, also known as Black Panther the heir of the throne of what country?", "What was Peter Parker doing when he was bitten by the radioactive spider?", "In the X-Men series, who established and ran the school for the mutants?", "In Thor: Ragnarok, who is Thor's opponent in the gladiator ring on the planet Sakaar?],
+  answerArray: [
+                ["Hulk", "Thor", "Spider-Man", "Wolverine"], ["United States", "Russia", "Wakanda", "Germany"], ["He was on a field trip", "He was dissecting the spider", "He was trying to stomp on the spider", "He was stealing the spider"], ["Wolverine", "Magneto", "Dr. Stephen Strange", "Professor Charles Xavier"], ["Loki", "Thanos", "Hela", "Hulk"],],
+  correctAnswers: [
+                  "D. Wolverine", "C. Wakanda", "A. He was on a field trip", "D. Professor Charles Xavier", "D.Hulk"],
+  imageArray: [
+              "<img class='center-block img-right' src='assets/images/ibjjf-1996.png'>", "<img class='center-block img-right' src='assets/images/adcc.png'>", "<img class='center-block img-right' src='assets/images/japan.jpg'>", "<img class='center-block img-right' src='assets/images/mata-leon.jpg'>", "<img class='center-block img-right' src='assets/images/passing-guard.jpeg'>"],
+  clock: "",
+  questionCounter: 0,
+  timeCounter: 20,
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function submitAnswer() {
-
-
-
-
-  console.log(radios);
-
-  for (var i = 0; i < 4; i++) {
-    var radios = $('input[name=choice]:checked', "#myForm" + (i + 1)).val();
-    console.log(radios);
-  }
-
-  // Set the date we're counting down to
-  var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
-
-  // Update the count down every 1 second
-  var x = setInterval(function () {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Output the result in an element with id="demo"
-    document.getElementById("seconds").innerHTML = seconds + "s";
-
-    // If the count down is over, write some text 
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("seconds").innerHTML = "EXPIRED";
+  function startScreen(){
+    //Create the start button
+    trivia.initialScreen = "<p class='text-center main-button'><a class='btn btn-primary btn-lg start-button text-center' href='#'>Start!</a></p>";
+    //Add Start button to main-area
+    $(".main-area").html(trivia.initialScreen);
+  };
+  
+  function timer(){
+    trivia.clock = setInterval(twentySeconds, 1000);
+    function twentySeconds(){
+      if(trivia.timeCounter === 0){
+        timeOutLoss();
+        clearInterval(trivia.clock);
+      }
+      if(trivia.timeCounter > 0) {
+        trivia.timeCounter --;
+      }
+      $(".timer").html(trivia.timeCounter);
     }
-  }, 1000);
+  };
+  
+  function wait(){
+    if(trivia.questionCounter < 4) {
+      trivia.questionCounter ++;
+      generateHTML();
+      trivia.timeCounter = 20;
+      timer();
+    }
+    else {
+      finalScreen();
+    }
+  };
+  
+  function win(){
+    trivia.correctCounter ++;
+    trivia.gameHTML = "<p class='text-center'> Time Remaining: <span class='timer'>" + trivia.timeCounter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + trivia.correctAnswers[trivia.questionCounter] + "</p>" + trivia.imageArray[trivia.questionCounter];
+    $(".main-area").html(trivia.gameHTML);
+    setTimeout(wait, 4000);
+  };
+  
+  function loss(){
+    trivia.inCorrectCounter ++;
+    trivia.gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + trivia.timeCounter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ trivia.correctAnswers[trivia.questionCounter] + "</p>" + trivia.imageArray[trivia.questionCounter];
+    $(".main-area").html(trivia.gameHTML);
+    setTimeout(wait, 4000);
+  };
+  
+  function timeOutLoss(){
+    trivia.unAnsweredCounter ++;
+    trivia.gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + trivia.timeCounter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + trivia.correctAnswers[trivia.questionCounter] + "</p>" + trivia.imageArray[trivia.questionCounter];
+    $(".main-area").html(trivia.gameHTML);
+    setTimeout(wait, 4000);
+  };
+  
+  function finalScreen(){
+    trivia.gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + trivia.timeCounter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + trivia.correctCounter + "</p>" + "<p>Wrong Answers: " + trivia.inCorrectCounter + "</p>" + "<p>Unanswered: " + trivia.unAnsweredCounter + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+    $(".main-area").html(trivia.gameHTML);
+  };
+  
+  function resetGame(){
+    trivia.questionCounter = 0;
+    trivia.correctCounter = 0;
+    trivia.inCorrectCounter = 0;
+    trivia.unAnsweredCounter = 0;
+    trivia.timeCounter = 20;
+    generateHTML();
+    timer();
+  };
+  
+  function generateHTML(){
+    trivia.gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>20</span></p><p class='text-center'>" + trivia.questionsArray[trivia.questionCounter] + "</p><button class='first-answer answer'>A. " + trivia.answerArray[trivia.questionCounter][0] + "</button><br><button class='answer'>B. "+trivia.answerArray[trivia.questionCounter][1]+"</button><br><button class='answer'>C. "+trivia.answerArray[trivia.questionCounter][2]+"</button><br><button class='answer'>D. "+trivia.answerArray[trivia.questionCounter][3]+"</button>";
+    $(".main-area").html(trivia.gameHTML);
+  }
+  
